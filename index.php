c1bae56902e1e85f7be1e78b80109666778a6e57
asheshmagar <asheshmagar8888667@gmail.com>
2020-04-01 19:34:23 +0545

    Edited - index.php showing the content

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php include "includes/header.php"; ?>
-    <h2>You Might also Like</h2>
+    <h2 class="page-header">You Might also Like</h2>
     <div class="grid-view-container">
         <?php
-            $albumQuery = mysqli_query($con,"select * from albums");
+            $albumQuery = mysqli_query($con,"select * from albums order  by rand() limit 10");
             while($row = mysqli_fetch_array($albumQuery)){
-                echo $row['title'] . "<br>";
+
+                echo "<div class='grid-view-item'>
+                            <a href='album.php?id=" . $row['id'] . "' />
+                                <img src='" .$row['artworkPath'] ."' />
+                                <div class='grid-view-info'>"
+                                    . $row['title'] .
+                                "</div>
+                       </div>";
+
             }
         ?>
     </div>
