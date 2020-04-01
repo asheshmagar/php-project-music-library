a2ac3dde6986a39b9cdabe62cd9c745c04128758
asheshmagar <asheshmagar8888667@gmail.com>
2020-04-01 17:29:04 +0545

    Refactored_code - made code cleaner

--- includes/navBarContainer.php
+++ includes/navBarContainer.php
@@ -1,7 +1,25 @@
-<?php
-
-
-class navBarContainer
-{
-
-}
+<div id="navBarContainer">
+    <nav class="nav-bar">
+        <a href="../index.php" class="logo">
+            <img src="assets/images/icons/logo.png" alt="">
+        </a>
+        <div class="group">
+            <div class="group-navItem">
+                <a href="search.php" class="navItem__link">Search
+                    <img src="assets/images/icons/search.png" class="icon" alt="search">
+                </a>
+            </div>
+        </div>
+        <div class="group">
+            <div class="group-navItem">
+                <a href="browse.php" class="navItem__link">Browse</a>
+            </div>
+            <div class="group-navItem">
+                <a href="music.php" class="navItem__link">Your Music</a>
+            </div>
+            <div class="group-navItem">
+                <a href="profile.php" class="navItem__link">Ashesh Magar</a>
+            </div>
+        </div>
+    </nav>
+</div>
---
+++ includes/nowPlayingBar.php
@@ -0,0 +1,81 @@
+<div id="nowPlatingBarContainer">
+    <div id="nowPlayingBar">
+
+        <div id="nowPlayingLeft">
+            <div class="content">
+                    <span class="album-link">
+                        <img src="http://localhost/php-project-music-library/assets/images/cover.png" class="album-artwork" alt="">
+                    </span>
+                <div class="track-info">
+                        <span class="track-name">
+                            <span>Wendy 👌✌</span>
+                        </span>
+                    <span class="artist-name">
+                            <span>Shotgun Willy</span>
+                        </span>
+                </div>
+            </div>
+        </div>
+        <div id="nowPlayingCenter">
+            <div class="content playerControls">
+                <div class="buttons">
+                    <button class="control-button shuffle" title="Shuffle Songs">
+                        <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                    </button>
+
+
+                    <button class="control-button back" title="Previous Song">
+                        <img src="assets/images/icons/back.png" alt="Previous">
+                    </button>
+
+
+                    <button class="control-button play" title="Play">
+                        <img src="assets/images/icons/play.png" alt="Play">
+                    </button>
+
+                    <button class="control-button pause" title="Play" style="display: none">
+                        <img src="assets/images/icons/pause.png" alt="Pause">
+                    </button>
+
+                    <button class="control-button next" title="Next Song">
+                        <img src="assets/images/icons/next.png" alt="Next">
+                    </button>
+
+                    <button class="control-button repeat" title="Repeat Songs">
+                        <img src="assets/images/icons/repeat.png" alt="Repeat">
+                    </button>
+
+                </div>
+            </div>
+            <div class="playbackBar">
+                        <span class="progressTime current">
+                            0.00
+                        </span>
+                <div class="progressBar">
+                    <div class="progressBar-bg">
+                        <div class="progress">
+
+                        </div>
+                    </div>
+                </div>
+                <span class="progressTime remaining">
+                            0.00
+                        </span>
+            </div>
+        </div>
+        <div id="nowPlayingRight">
+            <div class="volume-bar">
+                <button class="control-button volume" title="volume">
+                    <img src="assets/images/icons/sound.png" alt="volume">
+                </button>
+                <div class="progressBar">
+                    <div class="progressBar-bg">
+                        <div class="progress">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
